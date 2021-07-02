cda5aa21e731ecb6e8c7c5738fc7f795741ed25b
mrWeiss <giorgio2197@gmail.com>
2021-07-02 12:46:03 +0200

    update interface

--- ts_proj/lib/poliesami.d.ts
+++ ts_proj/lib/poliesami.d.ts
@@ -2,11 +2,27 @@ declare namespace PoliEsaMi {
 	class Model {
 		get identity(): Identity | null;
 
-		async login(user: string, password: string, keep: boolean = false): Promise<APIResponse<Identity>>;
+		login(user: string, password: string, keep?: boolean): Promise<APIResponse<Identity>>;
 		logout(): void;
 
-		async test(): Promise<APIResponse<{"message": string}>>;
-		async testInside(): Promise<APIResponse<{"message": string}>>;
+		test(): Promise<APIResponse<{"message": string}>>;
+		testInside(): Promise<APIResponse<{"message": string}>>;
+
+		getStudCoursesExams(studentId: number, year?: number): Promise<APIResponse<CourseExams[]>>;
+		getStudExam(studentId: number, examId: number): Promise<APIResponse<ExamCourse>>;
+		getStudExamReg(studentId: number, examId: number): Promise<APIResponse<ExamRegistration | null>>;
+		registerStudToExamReg(studentId: number, examId: number): Promise<APIResponse<boolean>>;
+		deregisterStudFromExamReg(studentId: number, examId: number): Promise<APIResponse<boolean>>;
+		rejectStudExamReg(studentId: number, examId: number): Promise<APIResponse<boolean>>;
+
+		getProfCoursesExams(professorId: number, year?: number): Promise<APIResponse<CourseExams[]>>;
+		getProfExam(professorId: number, examId: number): Promise<APIResponse<ExamCourse>>;
+		getProfExamRegistrations(professorId: number, examId: number): Promise<APIResponse<ExamRegistrationCareer[]>>;
+		getProfExamRegistration(professorId: number, examId: number, studentId: number): Promise<APIResponse<ExamRegistrationCareer>>;
+		getProfExamRecords(professorId: number, examId: number): Promise<APIResponse<ExamRecord[]>>;
+		publishProfExamRegistrations(professorId: number, examId: number): Promise<APIResponse<boolean>>;
+		verbalizeProfExamRegistrations(professorId: number, examId: number): Promise<APIResponse<boolean>>;
+		editProfExamRegistrations(professorId: number, examId: number, examEvaluations: ExamEvaluation[]): Promise<APIResponse<boolean>>;
 	}
 
 	const storage: Storage;
@@ -39,6 +55,60 @@ interface Career {
 	id: number;
 	role: string;
 	major?: string;
+}
+
+interface Course {
+	id: number;
+	name: string;
+	semester: string;
+	cfu: number;
+	year: number;
+	professorId: number;
+}
+
+interface CourseExams extends Course {
+	exams: Exam[];
+}
+
+interface Exam {
+	id: number;
+	year: number;
+	date: string;
+}
+
+interface ExamCourse extends Exam {
+	course: Course;
+}
+
+type ExamStatus = 'NINS' | 'INS' | 'PUB' | 'RIF' | 'VERB';
+type ExamResult = 'VUOTO' | 'ASS' | 'RM' | 'RP' | 'PASS';
+
+interface ExamRegistration {
+	studentId: number;
+	examId: number;
+	status: ExamStatus;
+	result: ExamResult;
+	grade: number;
+	laude: boolean;
+	resultRepresentation: string;
+	recordId: number;
+}
+
+interface CareerUser extends Career {
+	user: User;
+}
+
+interface ExamRegistrationCareer extends ExamRegistration {
+	career: CareerUser;
+}
+
+type ExamEvaluation = [number, ExamResult, number, boolean];
+
+interface ExamRecord {
+	id: number;
+	examId: number;
+	time: string;
+	examRegistrations: ExamRegistrationCareer[];
 }
 
 interface Storage {
